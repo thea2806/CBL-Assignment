67d4f4a439d5a150a877f38585b80666dffd029b
Theodora-Maria Goranel <thea.goranel@gmail.com>
2023-10-15 16:42:33 +0200

    Test 1 passed

--- Cell.java
+++ Cell.java
@@ -1,6 +1,6 @@
 public class Cell {
-    private int row;
-    private int column;
+    int row;
+    int column;
     boolean hasMine; //De vzt daca se poate altcumva
 
     public Cell(int row, int column, boolean hasMine) {
--- MinesweeperBoard.java
+++ MinesweeperBoard.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class MinesweeperBoard {
-    Cell[][] board;
+    Cell[][] board = new Cell[100][100];
     int sizeRow;
     int sizeColumn;
 
@@ -11,7 +11,7 @@ public class MinesweeperBoard {
                 board[i][j] = new Cell(i, j, false);
             }
         }
-        this.sizeRow =sizeRow;
+        this.sizeRow = sizeRow;
         this.sizeColumn = sizeColumn;
     }
 
@@ -22,16 +22,37 @@ public class MinesweeperBoard {
         for (int i = 0; i < minesNum; i++) {
             int row;
             int col;
-            do{
+            do {
                 row = random.nextInt(rows);
                 col = random.nextInt(columns);
-                
-            } while(board[row][col].hasMine);
+
+            } while (board[row][col].hasMine);
             board[row][col].hasMine = true;
         }
     }
 
     public int neighborMines(Cell cell) {
-        return 0;
-    }
+        int sizeRow2 = sizeRow + 2;
+        int sizeColumn2 = sizeColumn + 2;
+        int i;
+        int j;
+        int neighborMines = 0;
+        int[][] boardCopy = new int[sizeRow2][sizeColumn2];
+    
+        for (i = 0; i < sizeRow; i++) {
+            for (j = 0; j < sizeColumn; j++) {
+                if (board[i][j].hasMine) {
+                    boardCopy[i + 1][j + 1] = 1;
+                }
+            }
+        }
+    
+        for (i = cell.row; i <= cell.row + 2; i++) {
+            for (j = cell.column; j <= cell.column + 2; j++) {
+                neighborMines += boardCopy[i][j];
+            }
+        }
+        return neighborMines;
+}
+
 }
